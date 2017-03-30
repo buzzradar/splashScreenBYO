9968bf18dc141a6bd07792f0e180d3b8c5ae93e8
juan_infante <j.infante.garcia@gmail.com>
2017-03-30 12:19:11 +0100

    updating the new copy with dashed dots

--- app/src/js/controllers/preview/copy-ctrl.js
+++ app/src/js/controllers/preview/copy-ctrl.js
@@ -59,42 +59,106 @@ function _loadCopy() {
       let groupText = this.allCopyGroup.append("g");
       groupText.attr('class', 'copy-group');
 
-      let txtNode = groupText.selectAll("text").data([ {"x":0, "y":0, initX: Number(item.x), initY:Number(item.y), "copy" : item.copy, svg:this.svgContainer} ]).enter().append("text")
-        .attr('x', 0 )   //I will change the x coordinate later on when I find out how much is the width of the text. Line 97
-        .attr('y', function(d, i){ return Number(item.y)+(30 + i * 90); })
-        .attr('text-anchor', "start")
-        .attr("font-size", Number(item.size) )
-        .attr("fill", '#'+item.colour)
-        .attr('index', key)
-        .text(function(d){ return d.copy; })
-        //We use this function to wrap the text within the given width.
-        .call(_wrapCopyWidth, Number(item.width))
-        .call(d3.drag()
 
-          .on("drag", function(d,i) {
+      //Rectangle
+      this.dottedRect = groupText.append("rect")
+                  .attr("x", 0)
+                  .attr("y", 0)
+                  .attr("width", Number(item.width))
+                  .attr("height", 45)
+                  .style("stroke", '#ffffff')
+                  .style("fill", "none")
+                  .style("stroke-width", 5)
+                  .style("stroke-dasharray", ("20, 20"))
 
-            if (self.isDragable) {
+      //text
+      let txtNode = groupText.append("text")
+                  .attr("dx", 0)
+                  .attr("dy", 0)
+                  .attr('text-anchor', "start")
+                  .attr("font-size", Number(item.size) )
+                  .attr("fill", '#'+item.colour)
+                  .attr('index', key)
+                  .attr('alignment-baseline', 'hanging')
+                  .text(item.copy)
 
-              d.x += d3.event.dx
-              d.y += d3.event.dy
+      //Get width of the text and then change the X coordinate.
+      let txtWidth = txtNode.node().getBBox().width;
+      txtNode.attr("transform", "translate("+_getXpos(item,txtWidth)+",0)");
 
-              let newX = Math.round( DisplayGlobals_SRV.scaleRatio(d.x) + d.initX );
-              let newY = Math.round( DisplayGlobals_SRV.scaleRatio(d.y) + d.initY );
+      //Transform the position of the groupText
+      groupText.attr("transform", "translate("+Number(item.x)+","+Number(item.y)+")");
 
-              DisplayGlobals_SRV.getEditorRef().updateCopyPosition(newX,newY,Number($(this).attr('index')));
-              DisplayGlobals_SRV.getPreviewRef().updateChanges(true);
+      //Add drag functionality
+      groupText.call(d3.drag().on("drag", function(d,i) {
 
-              d3.select(this).attr("transform", function(d,i){
-                  return "translate(" + [ DisplayGlobals_SRV.scaleRatio(d.x) ,DisplayGlobals_SRV.scaleRatio(d.y) ] + ")"  
-              })
+        if (self.isDragable) {
 
-            }
+          let coordinates = _getTransformValues(d3.select(this));
+          console.log(coordinates);
+          console.log(d3.event.x);
+          console.log(d3.event.y);
+
+          let newX = d3.event.x;
+          let newY = d3.event.y;
+
+          // let newX = Math.round( DisplayGlobals_SRV.scaleRatio(dx) + 0);
+          // let newY = Math.round( DisplayGlobals_SRV.scaleRatio(dy) + 0);
+
+          // DisplayGlobals_SRV.getEditorRef().updateCopyPosition(newX,newY,Number($(this).attr('index')));
+          // DisplayGlobals_SRV.getPreviewRef().updateChanges(true);
+
+          d3.select(this).attr("transform", function(d,i){
+              return "translate(" + [ DisplayGlobals_SRV.scaleRatio(newX) ,DisplayGlobals_SRV.scaleRatio(newY) ] + ")"  
+          })
+
+        }
+          
+      }));
+
+
+
+
+
+
+
+      // let txtNode = groupText.selectAll("text").data([ {"x":0, "y":0, initX: Number(item.x), initY:Number(item.y), "copy" : item.copy, svg:this.svgContainer} ]).enter().append("text")
+      //   .attr('x', 0 )   //I will change the x coordinate later on when I find out how much is the width of the text. Line 97
+      //   .attr('y', function(d, i){ return Number(item.y)+(30 + i * 90); })
+      //   .attr('text-anchor', "start")
+      //   .attr("font-size", Number(item.size) )
+      //   .attr("fill", '#'+item.colour)
+      //   .attr('index', key)
+      //   .text(function(d){ return d.copy; })
+      //   //We use this function to wrap the text within the given width.
+      //   .call(_wrapCopyWidth, Number(item.width))
+      //   .call(d3.drag()
+
+      //     .on("drag", function(d,i) {
+
+      //       if (self.isDragable) {
+
+      //         d.x += d3.event.dx
+      //         d.y += d3.event.dy
+
+      //         let newX = Math.round( DisplayGlobals_SRV.scaleRatio(d.x) + d.initX);
+      //         let newY = Math.round( DisplayGlobals_SRV.scaleRatio(d.y) + d.initY);
+
+      //         DisplayGlobals_SRV.getEditorRef().updateCopyPosition(newX,newY,Number($(this).attr('index')));
+      //         DisplayGlobals_SRV.getPreviewRef().updateChanges(true);
+
+      //         d3.select(this).attr("transform", function(d,i){
+      //             return "translate(" + [ DisplayGlobals_SRV.scaleRatio(d.x) ,DisplayGlobals_SRV.scaleRatio(d.y) ] + ")"  
+      //         })
+
+      //       }
               
-          }));
+      //     }));
 
-        //Get width of the text and then change the X coordinate.
-        let txtWidth = txtNode.node().getBBox().width;
-        txtNode.attr("transform", "translate("+_getXpos(item,txtWidth)+",0)");
+      //   //Get width of the text and then change the X coordinate.
+      //   let txtWidth = txtNode.node().getBBox().width;
+      //   console.log(txtWidth)
+      //   txtNode.attr("transform", "translate("+_getXpos(item,txtWidth)+",0)");
 
     }
 
@@ -102,6 +166,21 @@ function _loadCopy() {
 
    _isDragable.call(this);
 
+
+}
+
+
+
+
+function _getTransformValues(d3Elem) {
+
+  let translateString = d3Elem.attr('transform');
+  translateString = translateString.replace("translate(", "");
+  translateString = translateString.replace(")", "");
+  let arrayCoordinates = translateString.split(",");
+  arrayCoordinates[0] = Number(arrayCoordinates[0]);
+  arrayCoordinates[1] = Number(arrayCoordinates[1]);
+  return arrayCoordinates;
 
 }
 
